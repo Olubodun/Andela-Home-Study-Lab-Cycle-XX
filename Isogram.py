f0f4d8c63f9fdf365acfb7d641be590c85909cb4
Olubodun <olubodunfemi@hotmail.com>
2017-02-27 15:19:49 +0100

    Create Isogram.py
    Intro to Programming Lab

---
+++ Isogram.py
@@ -0,0 +1,8 @@
+#This program checks a string of word without a repeating letter.
+def is_isogram(string):
+    if isinstance(string, str) and len(string) != 0:
+        string = string.lower()
+        return string, len(string) == len(set(string))
+    if not string:
+        return string, False;
+    raise TypeError('Argument should be a string')
