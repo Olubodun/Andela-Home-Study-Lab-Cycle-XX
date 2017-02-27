d1c50d9c111c65c260730d58f82f0dadbf71397d
Olubodun <olubodunfemi@hotmail.com>
2017-02-27 15:56:37 +0100

    Delete Isogram.py

--- Isogram.py
+++
@@ -1,8 +0,0 @@
-#This program checks if a string of word is without a repeating letter.
-def is_isogram(string):
-    if isinstance(string, str) and len(string) != 0:
-        string = string.lower()
-        return string, len(string) == len(set(string))
-    if not string:
-        return string, False;
-    raise TypeError('Argument should be a string')
